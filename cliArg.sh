56d3f9d7d5fd041ed057fabb952789dcb6716506
ethan-rod6 <ethanrodrigo06@gmail.com>
2022-02-28 21:03:01 +0530

    ...

--- cliArg.sh
+++ cliArg.sh
@@ -4,12 +4,12 @@ devices="/dev/$(lsblk -db | awk '/ 8:/' | awk '{print $1" "$4}' | sort -k 2 | ta
 let "rootSize = (($(lsblk -db | awk '/ 8:/' | awk '{print $1" "$4}' | sort -k 2 | tail -n1 | awk '{print $2}') * 10) / 100) / 1073741824" # default root size is the 10% of the actual size
 vgroupName="volgroup0" # the default volume group name
 
-getArg(){
-	if ! ARGUMENTS=$(getopt -a -n setuplvm -o r:v:d: --l root-size:,vgroup-name:,devices: -- "$@") # storing arguments in an array
-	then
-		exit 1
-	fi
+if ! ARGUMENTS=$(getopt -a -n setuplvm -o r:v:d: --l root-size:,vgroup-name:,devices: -- "$@") # storing arguments in an array
+then
+	exit 1
+fi
 
+getArg(){
 	eval set -- "$ARGUMENTS"
 	while [ $# -gt 0 ]
 	do
@@ -23,6 +23,8 @@ getArg(){
 		shift
 	done
 }
+
+getArg
 
 echo $rootSize
 echo $vgroupName
